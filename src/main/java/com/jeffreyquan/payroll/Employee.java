2fd0d2ce6e5d0364e623e3d05243876fcb3f5fad
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-07-16 21:05:34 +1000

    📝 added versioning

--- src/main/java/com/jeffreyquan/payroll/Employee.java
+++ src/main/java/com/jeffreyquan/payroll/Employee.java
@@ -1,17 +1,23 @@
 package com.jeffreyquan.payroll;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Version;
 import java.util.Objects;
 
 @Entity
 public class Employee {
 
-    private @Id @GeneratedValue  Long id;
+    private @Id @GeneratedValue Long id;
     private String firstName;
     private String lastName;
     private String description;
+
+    private @Version @JsonIgnore
+    Long version;
 
     private Employee() {}
 
@@ -26,12 +32,12 @@ public class Employee {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Employee employee = (Employee) o;
-        return Objects.equals(id, employee.id) && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName) && Objects.equals(description, employee.description);
+        return Objects.equals(id, employee.id) && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName) && Objects.equals(description, employee.description) && Objects.equals(version, employee.version);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, firstName, lastName, description);
+        return Objects.hash(id, firstName, lastName, description, version);
     }
 
     public Long getId() {
@@ -66,6 +72,14 @@ public class Employee {
         this.description = description;
     }
 
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
     @Override
     public String toString() {
         return "Employee{" +
@@ -73,6 +87,7 @@ public class Employee {
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", description='" + description + '\'' +
+                ", version=" + version +
                 '}';
     }
 }
